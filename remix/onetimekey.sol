0d751bb4e45ae260b954d7a183d1d04720b7b06b
cshapard26 <152207773+cshapard26@users.noreply.github.com>
2024-03-24 01:35:09 -0500

    Update onetimekey.sol

--- remix/onetimekey.sol
+++ remix/onetimekey.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
 contract OneTimeKey {
     address owner;
     mapping(address => bytes32) private keys;
     mapping(bytes32 => bool) public keyUsed;
+
+    address[] private theAddresses;
 
     event KeyIssued(address indexed to, bytes32 key);
     event KeyUsed(address indexed by, bytes32 key);
@@ -21,18 +24,26 @@ contract OneTimeKey {
     function issueKey(address _to, bytes32 _key) public onlyOwner {
         require(keys[_to] == 0, "Key already issued");
         keys[_to] = _key;
+        theAddresses.push(_to);
         emit KeyIssued(_to, _key);
+
     }
 
     function useKey(bytes32 _key) public {
-        require(keys[msg.sender] == _key, "Invalid key");
+        bool keyInList = false;
+        for (uint i = 0; i < theAddresses.length; i++) {
+            if (keys[theAddresses[i]] == _key) {
+                keyInList = true;
+            }
+        }
+        require(keyInList, "Invalid key");
         require(!keyUsed[_key], "Key already used");
         keyUsed[_key] = true;
 
         emit KeyUsed(msg.sender, _key);
 
-
         delete keys[msg.sender];
+
     }
 
     function getKeyStatus(address _user) public view returns (bool) {
