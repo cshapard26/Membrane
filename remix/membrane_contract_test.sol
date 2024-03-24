f5d09f2b190bdf6fc709f25d78c07cdad32148f8
cshapard26 <152207773+cshapard26@users.noreply.github.com>
2024-03-24 01:02:55 -0500

    Update membrane_contract_test.sol

--- remix/membrane_contract_test.sol
+++ remix/membrane_contract_test.sol
@@ -1,24 +1,49 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @custom:dev-run-script ../scripts/deploy_with_ethers.ts
+ */
+
+
 import "remix_tests.sol"; // this import is automatically injected by Remix.
 import "hardhat/console.sol";
 import "./_MembraneContract.sol";
 
 contract MembraneTest {
-    bytes serverKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCI1KiHgoR6IwBjvOUXpxyzJyhky9ECGggBjisl7WMe1ucxDFsfPf+n9ghCGleODU7NM/NwulIHfT/Juwbmrh/hNx+D8Cb4nbC6/tYUtKVs5d9qpC8K2FoNhX6GdEPx6pjweX4f8wM+5Ev1LU6YXa9udMjNYgpqSOggkv4uHTVYvQIDAQAB";
-    MembraneCrowdsourcing crowdsourceInstance;
+    string serverKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCI1KiHgoR6IwBjvOUXpxyzJyhky9ECGggBjisl7WMe1ucxDFsfPf+n9ghCGleODU7NM/NwulIHfT/Juwbmrh/hNx+D8Cb4nbC6/tYUtKVs5d9qpC8K2FoNhX6GdEPx6pjweX4f8wM+5Ev1LU6YXa9udMjNYgpqSOggkv4uHTVYvQIDAQAB";
+    MembraneCrowdsourcing membraneInstance;
 
-    bytes encryptedData;
-    bytes32 hashedData = "78016cea74c298162366b9f86bfc3b16";
+    bytes encryptedData = "Hello, world!";
+    bytes hashedData = bytes32ToBytes(keccak256(abi.encodePacked(encryptedData)));
 
     function beforeAll () public {
-        crowdsourceInstance.setServerPublicKey(serverKey);
+        console.log("Running beforeAll");
+        membraneInstance = new MembraneCrowdsourcing();
+        membraneInstance.setServerPublicKey(serverKey);
+        console.log("Success!");
     }
 
     function checkCommit() public {
-        console.log("Running checkWinningProposal");
-        crowdsourceInstance.commit(hashedData, hashedData);
-        Assert.equal(crowdsourceInstance.checkCommitReceived(), true, "commit should have been received.");
+        console.log("Running checkCommit");
+        Assert.equal(membraneInstance.checkCommitReceived(), false, "commit should not have been received yet.");
+        membraneInstance.commit(hashedData, hashedData);
+        Assert.equal(membraneInstance.checkCommitReceived(), true, "commit should have been received.");
+        console.log("Success!");
+    }
+
+    function checkReveal() public view {
+        console.log("Running checkReveal");
+        membraneInstance.reveal(encryptedData);
+        console.log("Success!");
+    }
+
+    function bytes32ToBytes(bytes32 data) public pure returns (bytes memory) {
+        bytes memory result = new bytes(32);
+        assembly {
+            mstore(add(result, 32), data)
+        }
+        return result;
     }
 }
